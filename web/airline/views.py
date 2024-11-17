a9d6e163ef5d7958662b4419e73772e8a35fc8b8
habibsain <habibsain25@gmail.com>
2024-11-17 05:55:57 +0000

    new model

--- web/airline/views.py
+++ web/airline/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from . models import passenger
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'airline/index.html')
+    passenger_list = passenger.objects.all()
+    return render(request, 'airline/index.html', {'passenger_list': passenger_list})
 
 def search(request):
     pass
